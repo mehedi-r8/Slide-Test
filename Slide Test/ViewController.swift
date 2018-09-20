3de28e0ad61d06d769c1206452f2bdcac6d6a5a4
Mehedi Right <mehedi.r8@mehedir8s-macbook-air.local>
2018-09-20 19:56:22 +0600

    Initial Commit

---
+++ Slide Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slide Test
+//
+//  Created by MEHEDI.R8 on 9/20/18.
+//  Copyright © 2018 mehedi.r8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
